f1ccdec559cf1e5874157987c348f471e2cf9815
enuesaa <nsrciog@gmail.com>
2023-11-01 13:37:42 +0900

    feat: use cobra

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("hey")
+	app := &cobra.Command{
+		Use:     "cpbuf",
+		Short:   "A CLI tool to copy and paste files",
+		Version: "0.0.1",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+
+	// disable default
+	app.SetHelpCommand(&cobra.Command{Hidden: true})
+	app.PersistentFlags().BoolP("help", "", false, "Show help information")
+	app.CompletionOptions.DisableDefaultCmd = true
+
+	app.Execute()
 }
