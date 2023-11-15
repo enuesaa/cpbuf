b84b66cb811baa344135484ad51befe8e52ffce0
nua <enuesaa@gmail.com>
2023-11-16 00:52:19 +0900

    chore: bump version

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := &cobra.Command{
 		Use:     "cpbuf",
 		Short:   "A CLI tool to copy and paste files.\n`cpbuf` uses buf-dir to save files temporarily.",
-		Version: "0.0.4",
+		Version: "0.0.5",
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.Help()
 		},
