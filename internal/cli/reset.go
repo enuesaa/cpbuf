a1d12f358f06c0c9a50dd591b6fbcb5b31d28795
nua <enuesaa@gmail.com>
2023-12-10 23:02:37 +0900

    feat(reset): remove argument of `reset` because its behavior is not matched to command name. `reset` makes us imagine `make empty` not `rm file`

--- internal/cli/reset.go
+++ internal/cli/reset.go
@@ -10,17 +10,9 @@ import (
 
 func CreateResetCmd(repos repository.Repos) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "reset [<filename>]",
-		Short: "Clear buffered file. If filename is not passed, clear all files in buf dir.",
+		Use:   "reset",
+		Short: "Clear buffered files",
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) > 0 {
-				filename := args[0]
-				if err := usecase.RemoveFileInBufDir(repos, filename); err != nil {
-					log.Fatalf("Error: failed to remove a file in buf dir.\n%s\n", err.Error())
-				}
-				return
-			}
-
 			if err := usecase.DeleteBufDir(repos); err != nil {
 				log.Fatalf("Error: failed to clear buf dir.\n%s\n", err.Error())
 			}
